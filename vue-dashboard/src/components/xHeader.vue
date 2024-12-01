de835e0eb2f63f1dc48b7fe3eff6cb4df920826e
Thiago Schwartz <67984677+thiagocanali@users.noreply.github.com>
2024-12-01 09:57:58 -0300

    att v2.6

--- vue-dashboard/src/components/xHeader.vue
+++ vue-dashboard/src/components/xHeader.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="w-full bg-gray-100 ">
-    <div class="flex justify-between items-center h-[50px]">header</div>    
+        <div class="flex justify-between items-center h-[50px]">
+            <div class="p-4 cursor-pointer hover:bg-gray-50">
+                <i class="pi pi-bars"></i>
+            </div>
+        </div>
     </div>
 
 </template>
--- vue-dashboard/src/main.js
+++ vue-dashboard/src/main.js
@@ -1,8 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+
 import './assets/app.css'
 import router from './router'
+import PrimeVue from 'primevue/config';
+
+import 'primevue/resources/themes/tailwind-light/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
 
 const app = createApp(App);
 app.use(router);
+app.use(PrimeVue);
+
+
 app.mount('#app')
