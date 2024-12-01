b417cb0049f9f9d324c9f669ab0af79931306967
Thiago Schwartz <67984677+thiagocanali@users.noreply.github.com>
2024-12-01 09:39:33 -0300

    att v2.5

--- vue-dashboard/src/components/xSidebar.vue
+++ vue-dashboard/src/components/xSidebar.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="h-screen w-[300px] bg-gray-800">
-sidebar
+        <div class="bg-gray-700 h-[50px]">
+            <div class="text-xl font-bold text-center flex items-center justify-center text-white h-full">APP DEV</div>
+        </div>
     </div>
 
 </template>
