0c2986ef1d86b9dad6f04f457970ebfae02ae766
Thiago Schwartz <67984677+thiagocanali@users.noreply.github.com>
2024-12-01 10:15:48 -0300

    att v2.7

--- vue-dashboard/src/components/xHeader.vue
+++ vue-dashboard/src/components/xHeader.vue
@@ -1,8 +1,14 @@
 <template>
-    <div class="w-full bg-gray-100 ">
+    <div class="w-full bg-gray-100">
         <div class="flex justify-between items-center h-[50px]">
             <div class="p-4 cursor-pointer hover:bg-gray-50">
                 <i class="pi pi-bars"></i>
+            </div>
+            <div class="py-2">
+                <InputText type="text" v-model="value" class="h-[40px]" placeholder="Search..." />
+            </div>
+            <div>
+                <xAvatar icon="pi pi-user" class="mr-2" style="background-color: #9c27b0; color: #fff" shape="circle" />
             </div>
         </div>
     </div>
--- vue-dashboard/src/main.js
+++ vue-dashboard/src/main.js
@@ -1,15 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
 import './assets/app.css'
 import router from './router'
 import PrimeVue from 'primevue/config';
-
 import 'primevue/resources/themes/tailwind-light/theme.css';
 import 'primevue/resources/primevue.min.css';
 import 'primeicons/primeicons.css';
+import InputText from 'primevue/inputtext';
+import Avatar from 'primevue/avatar';
 
 const app = createApp(App);
+
+app.component('InputText', InputText);
+app.component('xAvatar', Avatar);
 app.use(router);
 app.use(PrimeVue);
 
